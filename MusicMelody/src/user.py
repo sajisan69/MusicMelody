9d16dedbd6f03fadfe612c72084c9a31476f2b29
Sabbir Ahmed Jisan <jisancse875@gmail.com>
2025-12-23 15:08:10 +0600

    Enhance User class with default username and mood methods
    Added default username as 'Guest' and methods to manage mood text.

--- MusicMelody/src/user.py
+++ MusicMelody/src/user.py
@@ -1,8 +1,12 @@
 class User:
-    def __init__(self, username, age=None, favorite_genres=None):
+    # 1. We added '="Guest"' so you can create a user without providing a name immediately.
+    def __init__(self, username="Guest", age=None, favorite_genres=None):
         self.__username = username
         self.__age = age
         self.__favorite_genres = favorite_genres if favorite_genres else []
+
+        # 2. We added this to store the mood (Required by UIHandler)
+        self._current_mood_text = ""
 
     def get_username(self):
         return self.__username
@@ -16,3 +20,10 @@ class User:
     def add_genre(self, genre):
         if genre not in self.__favorite_genres:
             self.__favorite_genres.append(genre)
+
+    # 3. These methods were missing! The UI needs them to save your input.
+    def set_mood_text(self, text):
+        self._current_mood_text = text
+
+    def get_mood_text(self):
+        return self._current_mood_text
