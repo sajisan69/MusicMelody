d0aee056c7e87f333ecdb1e79dd4ad8cb8b24c5e
Sabbir Ahmed Jisan <jisancse875@gmail.com>
2026-01-14 02:05:50 +0600

    Refactor User class and add mood history functionality
    Refactored User class to simplify initialization and added mood history management.

--- MusicMelody/src/user.py
+++ MusicMelody/src/user.py
@@ -1,29 +1,15 @@
 class User:
-    # 1. We added '="Guest"' so you can create a user without providing a name immediately.
-    def __init__(self, username="Guest", age=None, favorite_genres=None):
-        self.__username = username
-        self.__age = age
-        self.__favorite_genres = favorite_genres if favorite_genres else []
+    def __init__(self, username):
+        self.username = username
+        self.mood_history = []
 
-        # 2. We added this to store the mood (Required by UIHandler)
-        self._current_mood_text = ""
+    def add_mood_to_history(self, mood):
+        """Adds a mood to the history list."""
+        if mood and mood != "Neutral":
+            self.mood_history.append(mood)
+            # Keep only last 20 entries to save memory
+            if len(self.mood_history) > 20:
+                self.mood_history.pop(0)
 
-    def get_username(self):
-        return self.__username
-
-    def get_age(self):
-        return self.__age
-
-    def get_favorite_genres(self):
-        return self.__favorite_genres
-
-    def add_genre(self, genre):
-        if genre not in self.__favorite_genres:
-            self.__favorite_genres.append(genre)
-
-    # 3. These methods were missing! The UI needs them to save your input.
-    def set_mood_text(self, text):
-        self._current_mood_text = text
-
-    def get_mood_text(self):
-        return self._current_mood_text
+    def get_history(self):
+        return self.mood_history
